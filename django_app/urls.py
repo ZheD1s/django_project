502406ac70b643acdbbe04bbbed323b61ce49430
Zhetpissov Dias <zhed1s@mail.ru>
2023-01-12 22:38:10 +0600

    ListView and template

---
+++ django_app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.views.generic.base import TemplateView
+from .views import *
+
+urlpatterns = [
+    # path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', ProductListView.as_view(), name='home')
+]
+
+# path(<url:str>, <view:<class:view>>, name=<name:str>)
